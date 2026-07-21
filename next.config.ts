beb11c496696b19c519d951e3af9734fd5a5c85d
X1ng1 <chenjessicany@gmail.com>
2026-07-21 18:27:14 -0400

    configured deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  basePath: "/x1ng1-portfolio",
+  assetPrefix: "/x1ng1-portfolio/",
 };
 
 export default nextConfig;
